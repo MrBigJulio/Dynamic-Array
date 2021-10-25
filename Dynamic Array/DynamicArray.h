67104031d428be8a9835060d3da4bd04fe853d03
Vadim Savelyev <mrbigjulio@gmail.com>
2021-10-25 19:59:26 +0200

    Test

--- Dynamic Array/DynamicArray.h
+++ Dynamic Array/DynamicArray.h
@@ -2,11 +2,6 @@
 #define DYNAMICARRAY_H
 using namespace std;
 
-class Record {
-public:
-	int data;
-};
-
 template <class T>
 class Array {
 private:
@@ -18,42 +13,71 @@ private:
 
 public:
 	Array();
-	void resize(const Array&);
-	~Array();
-	const Array operator=(const Array&);
+	Array(const Array&);
+	//const Array operator=(const Array&);
+	T& operator[](const T&);
+	void resize();
 };
 template <class T>
 Array<T>::Array() {
 	arraySize = startArraySize;
 	ar = new T[arraySize];
-	ar->data = 0;
+	*ar = 0;
 }
 
 template <class T>
-void Array<T>::resize(const Array& obj) {
-	arraySize = obj.arraySize * resizeCoefficent;
-	*ar = new T[arraySize];
+ Array<T>::Array(const Array& obj) {
+	arraySize = obj.arraySize;
+	numberOfRecords = obj.numberOfRecords;
+	ar = new T[arraySize];
 	for (int i = 0; i < arraySize; i++) {
-		if (i < obj.arraySize) {
-			ar[i].data = obj.ar[i].data;
-		}
-		else
-		{
-			ar[i].data = 0;
-		}
+		*(ar + i) = *(obj.ar + i);
 	}
 }
 
-template <class T>
+/*template <class T>
 const Array<T> Array<T>::operator=(const Array& right) {
 	if (this != right) {
 		delete[] ar;
 		arraySize = right.arraySize;
 		ar = new T[arraySize];
 		for (int i = 0; i < arraySize; i++) {
-			(ar->data + i) = (right.ar->data = i);
+			*(ar + i) = *(right.ar = i);
 		}
 		return *this;
 	}
+};*/
+
+template <class T>
+T& Array<T>::operator[](const T& sub) {
+	if (sub < 0) {
+		cout << "Error! Can't be < 0" << endl;
+	}
+	else if (sub >= arraySize) {
+		resize();
+		return ar[sub];
+	}
+	else {
+		return ar[sub];
+	}
 };
+
+template <class T>
+void Array<T>::resize() {
+	Array* copyArray = new Array(*this);
+	arraySize = copyArray->arraySize * resizeCoefficent;
+	numberOfRecords = copyArray->numberOfRecords;
+	delete ar;
+	ar = new T[arraySize];
+	for (int i = 0; i < arraySize; i++) {
+		if (i < copyArray->arraySize) {
+			*(ar + i) = *(copyArray->ar + i);
+		}
+		else
+		{
+			*(ar + i) = 0;
+		}
+	}
+	delete copyArray;
+}
 #endif // !DYNAMICARRAY_H
--- Dynamic Array/main.cpp
+++ Dynamic Array/main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    Array<Record>* newArray = new Array<Record>();
-    newArray->resize(*newArray);
+    Array<int> newArray;
+    Array<int> newArray2;
+    newArray[0] = 1;
+    newArray[1] = 2;
+    newArray[2] = 3;
+    newArray[3] = 4;
+    newArray[4] = 5;
+    newArray[5] = 6;
+
+    cout << newArray[0] << endl;
+    cout << newArray[1] << endl;
+    cout << newArray[2] << endl;
+    cout << newArray[3] << endl;
+    cout << newArray[4] << endl;
+    cout << newArray[5] << endl;
+
 }
